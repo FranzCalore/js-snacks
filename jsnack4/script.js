0220d0337f1cfdd84cd37a6126943cee31da8a1b
FranzCalore <franzkaiser1993@gmail.com>
2022-11-14 16:19:27 +0100

    imposto un controllo sul primo carattere

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -1,3 +1,32 @@
 /* In un array sono contenuti i nomi degli invitati alla festa del grande
 Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o
 no alla festa. */
+
+let invitati = ["bryan", "emanuele", "monica", "sayori", "yuri"];
+
+let nomeInvitato = prompt ("Inserisci il tuo nome");
+
+let nomeONumero = nomeInvitato.charAt(0)
+
+nomeONumero = isNaN(nomeONumero)
+
+if (nomeONumero){
+
+nomeInvitato = nomeInvitato.toLowerCase();
+
+let flag=false;
+
+for (i=0; i<invitati.length; i++){
+    if (nomeInvitato==invitati[i]){
+        flag=true;
+    }
+}
+
+if (flag){
+    console.log("Puoi entrare, buon divertimento!")
+} else {
+    console.log ("Mi dispiace, il tuo nome non è sulla lista!")
+}
+} else {
+    console.log ("O sei figlio di Elon Musk o mi stai prendendo in giro!")
+}
